0d9e80c88eaa2c45669e56513203272317761a73
Luke Davies <lkdvs3@gmail.com>
2018-01-13 16:43:51 +0000

    Made builder use executible directory instead of current directory

--- Build/Program.cs
+++ Build/Program.cs
@@ -8,18 +8,25 @@ namespace Build
     {
         static void Main(string[] args)
         {
+            // set current directory to EXE path
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+
+            // load settings
             Settings settings = Settings.LoadSettings();
             Console.WriteLine();
 
+            // check if IN dir exists
             if (!Directory.Exists(settings.FileInPath))
             {
                 Console.WriteLine("File in path '{0}' not found, are you sure it exists? (relative directories allowed)", settings.FileInPath);
                 Pause();
                 return;
             }
+            // create OUT dir if it doesn't exist
             if (!Directory.Exists(settings.FileOutPath))
                 Directory.CreateDirectory(settings.FileOutPath);
 
+            // process files
             Processor.Process(settings);
 
 #if DEBUG
